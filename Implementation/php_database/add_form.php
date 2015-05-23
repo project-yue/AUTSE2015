79acd16e90df15996a5ec1a6c2ead3dd423cd88e
ben <benliangforloop@gmail.com>
2015-05-24 11:55:43 +1200

    ben php files

---
+++ Implementation/php_database/add_form.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+<title>Seler database insertion</title>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+</head>
+<body>
+<?php
+	// sql info or use include 'file.inc'
+       require_once('sql_info.php');
+	
+	// The @ operator suppresses the display of any error messages
+	// mysqli_connect returns false if connection failed, otherwise a connection value
+	$conn = @mysqli_connect($sql_host,
+		$sql_user,
+		$sql_pass,
+		$sql_db
+	);
+  
+	// Checks if connection is successful
+	if (!$conn) {
+		// Displays an error message, avoid using die() or exit() as this terminates the execution
+		// of the PHP script
+		echo "<p>Database connection failure</p>";
+	} else {
+		// Upon successful connection
+		
+		// Get data from the form
+		$username	= $_POST["teachername"];
+		$passworkd	= $_POST["teacherpass"];
+
+
+		// Set up the SQL command to add the data into the table
+		$query = "insert into $sql_tble"
+						."(username, password)"
+					." values "
+						."('$teachername','$teacherpass')";
+echo $query;
+		// executes the query
+		$result = mysqli_query($conn, $query);
+		// checks if the execution was successful
+		if(!$result) {
+			echo "<p>Something is wrong with ",	$query, "</p>";
+		} else {
+			// display an operation successful message
+			echo "<p>Success</p>";
+		} // if successful query operation
+
+		// close the database connection
+		mysqli_close($conn);
+	}  // if successful database connection
+?>
+</body>
+</html>
+
+
+
+
