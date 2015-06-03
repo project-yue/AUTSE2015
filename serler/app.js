8bc0680c0d2a2b86790522712cc764ec76a1e4f5
Yue Li <makingitbettergo@gmail.com>
2015-06-04 07:07:19 +1200

    added nodejs project

---
+++ serler/app.js
@@ -0,0 +1,112 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// // mongo db -start-
+// var mongo = require('mongodb');
+// // monk is used for connecting mongodb
+// var monk = require('monk');
+// // replace localhost with url if db running in a different place
+// var db = monk('localhost:27017/serlerdb');
+
+// mongo db -end-
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Make our db accessible to our router
+// app.use(function(req,res,next){
+//   req.db = db;
+//   next();
+//   console.log("mongo db is ready at port:27017.")
+// });
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.post('/hello', function(req, res){
+  alert("hi from express");
+});
+
+
+// db functions
+/* POST to Add User Service */
+// app.post('/', function(req, res) {
+//   // Set our internal DB variable
+//   var db = req.db;
+//
+//   // Get our form values. These rely on the "name" attributes
+//   alert(req.body);
+//   var userName = req.body.username;
+//   var userEmail = req.body.useremail;
+//
+//   // Set our collection
+//   var collection = db.get('usercollection');
+//
+//   // Submit to the DB
+//   collection.insert({
+//     "username" : userName,
+//     "email" : userEmail
+//   }, function (err, doc) {
+//     if (err) {
+//       // If it failed, return error
+//       res.send("There was a problem adding the information to the database.");
+//     }
+//     else {
+//       // If it worked, set the header so the address bar doesn't still say /adduser
+//       res.location("userlist");
+//       // And forward to success page
+//       res.redirect("userlist");
+//     }
+//   });
+// });
+
+module.exports = app;
